ca231a30e3d37f96b49555620a1f0437ee489e12
Dusty Leary <dleary@gmail.com>
2013-04-05 18:19:03 -0700

    fix make-symlinks.sh for vim

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -5,3 +5,6 @@ ln -sfn ~/.dotfiles/tmux.conf ~/.tmux.conf
 mkdir -p ~/Library/Preferences
 ln -sfn ~/.dotfiles/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 
+ln -sfn ~/.dotfiles/vim ~/.vim
+ln -sfn ~/.dotfiles/vim/vimrc ~/.vimrc
+
